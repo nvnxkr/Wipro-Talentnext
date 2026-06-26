05883c908e42b128eb3cffd8981467196e827690
nvnxkr <nvnkr2005@gmail.com>
2026-06-27 00:36:59 +0530

    CLA completed

---
+++ Python/4.Command Line Arguments/assignment_2.py
@@ -0,0 +1,24 @@
+'''
+Write a program to accept a welcome message through command line arguments and display
+the file name along with the welcome message.
+'''
+
+import sys
+import os
+
+def display_welcome():
+    if len(sys.argv) < 2:
+        print("Usage: python assignment_2.py <welcome_message...>")
+        return
+        
+    # The file name is the first argument in sys.argv
+    file_name = os.path.basename(sys.argv[0])
+    
+    # The welcome message is the rest of the arguments
+    welcome_message = " ".join(sys.argv[1:])
+    
+    print(f"File: {file_name}")
+    print(f"Message: {welcome_message}")
+
+if __name__ == "__main__":
+    display_welcome()
